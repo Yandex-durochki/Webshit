62c63a899b00950a23d145d4104cb92bb9a1363b
Silentam <121629848+Silentam@users.noreply.github.com>
2024-04-04 22:08:06 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 @app.route("/test")
 def test():
